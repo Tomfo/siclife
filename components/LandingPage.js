6a99c2b32b28fa7f3953fb30d888e2f293248db4
Tomfo <ekow.beduaddo@gmail.com>
2025-07-11 23:05:24 +0000

    image load

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -3,13 +3,22 @@ import Image from 'next/image';
 export default function LandingPage() {
 	return (
 		<>
-			<Image
+			{/* <Image
 				src='/sic.png'
 				alt='Logo'
 				width={600}
 				height={600}
 				className='rounded-full'
 				style={{ width: 600, height: 600 }}
+				unoptimized
+			/> */}
+			<Image
+				src='/moba.png'
+				alt='Logo'
+				width={36}
+				height={36}
+				className='rounded-full'
+				style={{ width: 36, height: 36 }}
 				unoptimized
 			/>
 		</>
